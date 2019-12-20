06348c5bd0991c1f1a8ab467a572f7027501211d
Jacob Dolph <jacob.dolph702@gmail.com>
2019-12-19 20:55:37 -0700

    made the ajax call give me data

--- script.js
+++ script.js
@@ -1,25 +1,45 @@
 
 
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=D3ghooGNpCU4Cqzg1bN6AfvYxIaMcNry"
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (data) {
-    console.log(data.response);
-    console.log(data.status);
-});
 
 
+function clear() {
+    $("#article-section").empty()
+};
+
+function buildQueryUrl() {
+    var key = "D3ghooGNpCU4Cqzg1bN6AfvYxIaMcNry"
+
+    var searchTerm = $("#search-term").val().trim()
+    var startYear = $("#start-year").val().trim()
+    if (parseInt(startYear)) {
+        startYear = startYear + "0101"
+    }
+    var endYear = $("#end-year").val().trim()
+    if (parseInt(endYear)) {
+        endYear = endYear + "1231"
+    }
+    var nyTimes = "https://api.nytimes.com/svc/search/v2/articlesearch.json?"
+    var url = nyTimes + "q=" + searchTerm + "&begin_date=" + startYear + "&end_date=" + endYear + "&api-key=" + key
+    console.log(url)
+    return url
+}
 
 var recordsEl = $("#article").val()
 
-$("#run-search").on("click", function () {
+$("#run-search").on("click", function (event) {
+    event.preventDefault()
+    clear();
+    var url = buildQueryUrl()
+    console.log(url)
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=D3ghooGNpCU4Cqzg1bN6AfvYxIaMcNry"
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (data) {
+        console.log(data.response);
 
-    var searchTerm = $("#search-term").val().trim()
-    console.log(searchTerm)
-    var startYear = $("#start-year").val().trim()
-    var endYear = $("#end-year").val().trim()
-    console.log(startYear)
-    console.log(endYear)
+    });
+
+
 })
+
